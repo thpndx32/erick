2bb474f5016f072403bc768bd28d762ac24bdc49
thpndx32 <62064823+thpndx32@users.noreply.github.com>
2020-05-02 23:05:15 -0500

    Add files via upload
    lab 4

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
+int gcd (int x, int y){
+    if (x < y){
+        int c = x;
+        x= y;
+        y = c;
+    }
+    if (y == 0) {
+        return x;
+    }
+    else {
+        return gcd(y, x%y );
+    }
+}
 int main()
 {
-    int x;
-    cout << "ingrese un numero" << endl;
-    cin >> x ;
-    int y = x;
-    int z = 1;
-    while (y >= 10){
-     y = y / 10;
-     z = z+1;
-    }
-    int a,b;
-    int c=z;
-    while (z != 0){
-        b = pow(10,(c-z+1));
-        a = (x % b)/(b/10);
-        cout << a<< " " ;
-        z=z-1;
-    }
+    cout << gcd (24,84)<< endl;
     return 0;
 }
